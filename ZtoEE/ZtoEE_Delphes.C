80f537da62a27e0c14134394781b1c7655d4ce47
edgarmsalazar <edgarmsalazarc@gmail.com>
2019-09-19 14:37:18 -0700

    Pretty labels 2

--- ZtoEE/ZtoEE_Delphes.C
+++ ZtoEE/ZtoEE_Delphes.C
@@ -29,9 +29,9 @@ void ZtoEE_Delphes(const char *inputFile)
   TClonesArray *branchElectron = treeReader->UseBranch("Electron");
     
   // Book histograms
-  TH1 *histMass = new TH1F("mass", "M_{inv}(#e_{1}, #E_{2})", 100, 40.0, 140.0);
-  TH1 *histPT1 = new TH1F("pt1", "PT(#e_{1})", 80, 0.0, 100.0);
-  TH1 *histPT2 = new TH1F("pt2", "PT(#E_{2})", 80, 0.0, 70.0);
+  TH1 *histMass = new TH1F("mass", "Invariant Mass (e_{1}e_{2})", 100, 40.0, 140.0);
+  TH1 *histPT1 = new TH1F("pt1", "e_{1}", 80, 0.0, 100.0);
+  TH1 *histPT2 = new TH1F("pt2", "e_{2}", 80, 0.0, 70.0);
 
   // Loop over all events
   for(Int_t entry = 0; entry < numberOfEntries; ++entry)
@@ -59,18 +59,26 @@ void ZtoEE_Delphes(const char *inputFile)
 
   // Show resulting histograms
   TCanvas *c1 = new TCanvas("c1", "c1");
-  histMass-> GetXaxis()->SetTitle("Mass [GeV]");
-  histMass-> GetYaxis()->SetTitle("Entries");
+  histMass-> GetXaxis()-> SetTitle("Mass [GeV]");
+  histMass-> GetYaxis()-> SetTitle("Entries");
+  histMass-> GetXaxis()-> SetTitleSize(0.05);
+  histMass-> GetYaxis()-> SetTitleSize(0.05);  
   histMass->Draw();
   c1 -> SaveAs("mass.png");
+  
   TCanvas *c2 = new TCanvas("c3", "c3");
   histPT1-> GetXaxis()->SetTitle("PT [GeV]");
   histPT1-> GetYaxis()->SetTitle("Entries");
+  histPT1-> GetXaxis()-> SetTitleSize(0.05);
+  histPT1-> GetYaxis()-> SetTitleSize(0.05);  
   histPT1 ->Draw();
   c2 -> SaveAs("pt1.png");
+  
   TCanvas *c3 = new TCanvas("c3", "c3");
   histPT2-> GetXaxis()->SetTitle("PT [GeV]");
   histPT2-> GetYaxis()->SetTitle("Entries");
+  histPT2-> GetXaxis()-> SetTitleSize(0.05);
+  histPT2-> GetYaxis()-> SetTitleSize(0.05);  
   histPT2 ->Draw();
   c3 -> SaveAs("pt2.png");
   
