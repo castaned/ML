6f7fa29e5b3e1609d5649385b2acc5d74d72ebd1
edgarmsalazar <edgarmsalazarc@gmail.com>
2019-10-02 15:46:14 -0700

    Added plots

--- WtoMuNu/WtoMuNu_Delphes.C
+++ WtoMuNu/WtoMuNu_Delphes.C
@@ -16,7 +16,7 @@ R__LOAD_LIBRARY(libDelphes.so)
 
 //------------------------------------------------------------------------------
 
-void ZtoEE_Delphes(const char *inputFile)
+void WtoMuNu_Delphes(const char *inputFile)
 {
   gSystem->Load("libDelphes");
 
@@ -28,15 +28,14 @@ void ZtoEE_Delphes(const char *inputFile)
   ExRootTreeReader *treeReader = new ExRootTreeReader(&chain);
   Long64_t numberOfEntries = treeReader->GetEntries();
 
-  TClonesArray *branchElectron = treeReader->UseBranch("Electron");
-    
+  TClonesArray *branchMuon = treeReader->UseBranch("Muon");
+  TClonesArray *branchJet = treeReader->UseBranch("Jet");
+  TClonesArray *branchMET = treeReader->UseBranch("MissingET");
+      
   // Book histograms
-  TH1 *histMass = new TH1F("mass", "Invariant Mass (e_{1}e_{2})", 100, 40.0, 140.0);
-  TH1 *histPT1 = new TH1F("pt1", "e_{1}", 80, 0.0, 100.0);
-  TH1 *histPT2 = new TH1F("pt2", "e_{2}", 80, 0.0, 70.0);
-  TH1 *histDR = new TH1F("dR", "#Delta R", 100, 0, 5);
-  
-  float dR_val;
+  TH1 *histMass = new TH1F("mass", "Invariant Mass of Jets", 80, 0.0, 1000.0);
+  TH1 *histPTmuon = new TH1F("pt", "Muon PT", 60, 0.0, 225.0);
+  TH1 *histMissET = new TH1F("miss", "Missing Energy", 40, 0.0, 160.0);
   
   // Loop over all events
   for(Int_t entry = 0; entry < numberOfEntries; ++entry)
@@ -44,68 +43,63 @@ void ZtoEE_Delphes(const char *inputFile)
     // Load selected branches with data from specified event
     treeReader->ReadEntry(entry);
 
-    Electron *e1, *e2;
-
-    // If event contains at least 2 electrons
-    if(branchElectron->GetEntries() > 1)
+    Muon *mu;
+    Jet *jet1, *jet2;
+    MissingET *nu;
+    
+    // Check if jets are sorted in decreasing order 
+    /*
+    for(Int_t k=0; k < branchJet->GetEntries(); ++k)
     {
-      // Take first two electrons
-      e1 = (Electron *) branchElectron->At(0);
-      e2 = (Electron  *) branchElectron->At(1);
-      dR_val = sqrt( pow((e2->Eta)-(e1->Eta),2) + pow((e2->Phi)-(e1->Phi),2) );
-
-      // Plot their invariant mass
-      histMass->Fill(((e1->P4()) + (e2->P4())).M());
-      histPT1 -> Fill(e1 -> PT);
-      histPT2 -> Fill(e2 -> PT);
-      histDR  -> Fill(dR_val);
-      
-      if (dR_val>3.13 && dR_val<3.15 ) {
-        cout << "Back2back. Event: " << entry << endl;
-      } else if (dR_val < 2.0) {
-        cout << "Parallel particles. Event: " << entry << endl;
-      }
-
+      jet1 = (Jet *) branchJet -> At(k);
+      cout << "Entry: " << entry << "\tJet PT: " << (jet1 -> PT) << endl;
     }
+    */
+    // YES they are!
 
+    // If event contains at least 2 jets and 1 muon
+    if(branchMuon->GetEntries() == 1 && branchJet->GetEntries() > 1)
+    {
+      mu = (Muon *) branchMuon -> At(0);
+      jet1 = (Jet *) branchJet -> At(0);
+      jet2 = (Jet *) branchJet -> At(1);
+      nu = (MissingET *) branchMET -> At(0);
 
+      // Plot jets invariant mass, muon PT, and MET.
+      histMass-> Fill(((jet1->P4()) + (jet2->P4())).M());
+      histPTmuon -> Fill(mu -> PT);
+      histMissET -> Fill(nu -> MET);
+      
+      if(mu->PT > 100 && nu->MET > 60 && ((jet1->P4()) + (jet2->P4())).M() > 80)
+      {
+        cout << "Entry: " << entry << endl;
+      }
+    }
   }
 
-  // Show resulting histograms
-  TFile *f = new TFile("mass.root", "recreate");
   TCanvas *c1 = new TCanvas("c1", "c1");
   histMass-> GetXaxis()-> SetTitle("Mass [GeV]");
   histMass-> GetYaxis()-> SetTitle("Entries");
   histMass-> GetXaxis()-> SetTitleSize(0.05);
   histMass-> GetYaxis()-> SetTitleSize(0.05);  
-  histMass->Draw();
-  histMass -> Write();
-  f -> Close();
-  c1 -> SaveAs("mass.png");
-  
-  TCanvas *c2 = new TCanvas("c3", "c3");
-  histPT1-> GetXaxis()->SetTitle("PT [GeV]");
-  histPT1-> GetYaxis()->SetTitle("Entries");
-  histPT1-> GetXaxis()-> SetTitleSize(0.05);
-  histPT1-> GetYaxis()-> SetTitleSize(0.05);  
-  histPT1 ->Draw();
-  c2 -> SaveAs("pt1.png");
+  histMass-> Draw();
+  c1 -> SaveAs("01_mass.png");
+
+  TCanvas *c2 = new TCanvas("c2", "c2");
+  histPTmuon-> GetXaxis()-> SetTitle("PT [GeV]");
+  histPTmuon-> GetYaxis()-> SetTitle("Entries");
+  histPTmuon-> GetXaxis()-> SetTitleSize(0.05);
+  histPTmuon-> GetYaxis()-> SetTitleSize(0.05);  
+  histPTmuon-> Draw();
+  c2 -> SaveAs("02_pt.png");
   
   TCanvas *c3 = new TCanvas("c3", "c3");
-  histPT2-> GetXaxis()->SetTitle("PT [GeV]");
-  histPT2-> GetYaxis()->SetTitle("Entries");
-  histPT2-> GetXaxis()-> SetTitleSize(0.05);
-  histPT2-> GetYaxis()-> SetTitleSize(0.05);  
-  histPT2 ->Draw();
-  c3 -> SaveAs("pt2.png");
-  
-  TCanvas *c4 = new TCanvas("c4", "c4");
-  histDR-> GetXaxis()->SetTitle("#Delta R");
-  histDR-> GetYaxis()->SetTitle("Entries");
-  histDR-> GetXaxis()-> SetTitleSize(0.05);
-  histDR-> GetYaxis()-> SetTitleSize(0.05);  
-  histDR ->Draw();
-  c4 -> SaveAs("dR.png");
-  
+  histMissET-> GetXaxis()-> SetTitle("Missing ET [GeV]");
+  histMissET-> GetYaxis()-> SetTitle("Entries");
+  histMissET-> GetXaxis()-> SetTitleSize(0.05);
+  histMissET-> GetYaxis()-> SetTitleSize(0.05);  
+  histMissET-> Draw();
+  c3 -> SaveAs("03_met.png");  
 }
+
 
